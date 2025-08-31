ee0ce55121d2e668f4fd20a651c8dc78d90a6013
joferdev2022 <fernando.lovaton.lopez@gmail.com>
2025-08-31 01:52:28 -0500

    resolve bug datetime 2

--- app/services/sales_service.py
+++ app/services/sales_service.py
@@ -44,7 +44,8 @@ async def add_sale(sale_data: dict) -> dict:
     
     sale_data["_id"] = ObjectId()
     
-    sale_data["fechaVenta"] = datetime.now(timezone.utc)
+    sale_data["fechaVenta"] = datetime.utcnow()
+    # sale_data["fechaVenta"] = datetime.now(timezone.utc)
     
     sale_data["precioTotalOriginal"] = sale_data["precioTotal"]
     # print(sale_data["fechaVenta"])
@@ -145,7 +146,7 @@ async def update_payment_by_id(sale_id: str, new_payment: float):
     return False
 
 async def get_daily_Sales_summary(local: int):
-    today = datetime.now(timezone.utc)
+    today = datetime.utcnow()
     start_day = datetime(today.year, today.month, today.day)
     end_day = start_day + timedelta(days=1)
 
