ee084129842af65e77ddf909a005e1bca183c4ab
joferdev2022 <fernando.lovaton.lopez@gmail.com>
2025-09-17 19:31:38 -0500

    solve bug privacy

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -10,9 +10,6 @@ MONGO_CLUSTER_NAME = settings.MONGO_CLUSTER_NAME
 # uri = f"mongodb+srv://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@{MONGO_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority"
 uri = "mongodb+srv://almacen:almacen@cluster0.3omflck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-# uri = "mongodb+srv://apilegal:apilegal@cluster0.3omflck.mongodb.net/?retryWrites=true&w=majority"
-# uri = "mongodb://apilegal:apilegal@mongodb:27017/"
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # client = MongoClient(uri)
 # Send a ping to confirm a successful connection
